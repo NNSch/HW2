129a9a733fe8550b1ee63ca976fd118de4f052cb
Natalia Shevchenko <ledidile@rambler.ru>
2023-06-21 13:27:26 +0300

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 9000; // цена за билет
-        int y = 20; // бонусные рубли равные одной мили
-        int z = x / y; // бонусные мили за билет
+        int  ticket = 9000; // цена за билет
+        int bonus = 20; // бонусные рубли равные одной мили
+        int miles = ticket / bonus; // бонусные мили за билет
 
-        System.out.println(z);
+        System.out.println(miles);
     }
 }
